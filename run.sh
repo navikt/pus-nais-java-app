7ae34bf84cdbb2eea3f69a5b0ade0181af5cd88b
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-11-09 18:59:11 +0100

    la til jvm-flagg for å følge containerens memory limits

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@ set -x
 
 # TODO ikke prodklart enda, men det kommer!
 
+# [ -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap ]
+# https://blogs.oracle.com/java-platform-group/java-se-support-for-docker-cpu-and-memory-limits
+
+
 java \
+-XX:+UnlockExperimentalVMOptions \
+-XX:+UseCGroupMemoryLimitForHeap \
 -server \
 -classpath "${APP_DIR}/WEB-INF/lib/*:${APP_DIR}/WEB-INF/classes" \
 Main
