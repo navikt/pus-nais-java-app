dfb8d0cbfdd8ceb5aaa5f4673dbff3da5c03acab
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-04-19 17:24:41 +0200

    aktiver appdynamics hvis APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY er satt

--- run.sh
+++ run.sh
@@ -2,12 +2,29 @@
 set -e
 set -x
 
+
+HOSTNAME=`hostname`
+
+# ikke aktiver appdynamic med mindre man har
+if [ "${APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY}" != "" ]; then
+
+APPDYNAMICS_OPTS="
+-javaagent:/appdynamics/javaagent.jar
+-Dappdynamics.agent.applicationName=${APP_NAME}
+-Dappdynamics.agent.nodeName=${HOSTNAME}
+-Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}
+"
+
+fi
+
+
 # [ -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap ]
 # https://blogs.oracle.com/java-platform-group/java-se-support-for-docker-cpu-and-memory-limits
 
 exec java \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
+${APPDYNAMICS_OPTS} \
 -server \
 -classpath "${APP_DIR}/WEB-INF/classes:${APP_DIR}/WEB-INF/lib/*" \
 Main
