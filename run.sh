6263838ad84d764609769ce41c254d9bd4ddb774
Peder Korsveien <peder.korsveien@nav.no>
2019-09-25 16:53:57 +0200

    Inject vault variables appended with .env

--- run.sh
+++ run.sh
@@ -17,6 +17,17 @@ APPDYNAMICS_OPTS="
 
 fi
 
+if test -d /var/run/secrets/nais.io/vault;
+then
+    for FILE in /var/run/secrets/nais.io/vault/*.env
+    do
+        for line in $(cat $FILE); do
+            echo "- exporting `echo $line | cut -d '=' -f 1`"
+            export $line
+        done
+    done
+fi
+
 # Convert proxy settings to Java form
 PROXY_OPTS=$(/proxyopts)
 
