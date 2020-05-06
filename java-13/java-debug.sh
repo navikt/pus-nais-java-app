3cb8fa04c7fbb0400077fab4b8c1079a9354a40f
Alexander Gård <alex.gaard@hotmail.com>
2020-05-06 16:06:56 +0200

    Ikke overskriv JAVA_OPTS når vi legger på debug opts

--- java-13/java-debug.sh
+++ java-13/java-debug.sh
@@ -3,5 +3,5 @@
 # Enables remote debugging on port 5005 if the application is running in one of the test clusters
 
 if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-sbs" ]; then
-  export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
+  export JAVA_OPTS="${JAVA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
 fi
